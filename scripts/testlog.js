7a219f5f4c768f9c6ebff386d0e0c72476848e06
Noah Lee <noahlee@wirelessprv-10-195-38-32.near.illinois.edu>
2017-12-04 14:40:30 -0600

    Added logging from storage

--- scripts/testlog.js
+++ scripts/testlog.js
@@ -52,6 +52,57 @@ function logLatestLocation(shippingData) {
     console.log('Latest Location was ' + shippingData.latestLocation.fullLocation);
 }
 
+/**
+ * Logs tracking number
+ * 
+ * @param {Object} shippingData data to look through for num
+ */
 function logTrackingNum(shippingData) {
     console.log('Tracking Number is ' + shippingData.trackingNumber);
+}
+
+/**
+ * Logs all contents of storage thoroughly for examination to help describe
+ * how to iterate on data
+ */
+function logStorage() {
+    chrome.storage.sync.get(null, (items) => { 
+        if(items !== undefined && Object.keys(items).length !== 0) {
+            for(var item in items) {
+                console.log('Name: ' + item);
+                console.log('Data: ');
+                console.log(items[item]);
+            }
+        }
+        else {
+            console.log('Storage is empty');
+        }
+    });
+}
+
+/**
+ * Logs all pickup dates in storage as demonstration of getting data from storage
+ */
+function logAllDates() {
+    chrome.storage.sync.get(null, (items) => {
+        if(items !== undefined && Object.keys(items).length !== 0) {
+            for(var item in items) {
+                console.log(item + ' pickup date was ' + items[item].date.fullDate);
+            }
+        }
+    });
+}
+
+/**
+ * Logs all package names in storage as demonstration of getting data from storage
+ */
+function logAllNames() {
+    chrome.storage.sync.get(null, (items) => {
+        if(items !== undefined && Object.keys(items).length !== 0) {
+            console.log('Packages in storage:');
+            for(var item in items) {
+                console.log(item);
+            }
+        }
+    });
 }
