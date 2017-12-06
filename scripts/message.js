312440f9228f7aaac17a88a99cb0209136f205b3
Haojia Yu <yuhaojia@wirelessprv-10-193-26-204.near.illinois.edu>
2017-12-05 22:06:28 -0600

    First Add about the message notification

--- scripts/message.js
+++ scripts/message.js
@@ -1,18 +1,28 @@
 function getMessage() {
     return {
-        "appid":"17949"
-        "to":"+18722020536"
-        "project":"OneClick"
-        "signature":"58c3e1edeb26bff1aa4cddc224f81714"
+        "appid":"17949",
+        "to":"+18722020536",
+        "project":"OneClick",
+        "signature":"58c3e1edeb26bff1aa4cddc224f81714",
     }
 }
 
 function makeListRequest() {
     var messageData = getMessage();
+    const corsproxy = "https://cors-anywhere.herokuapp.com/";
     const messageurl = "https://api.mysubmail.com/internationalsms/xsend.json";
     var httpRequest = new XMLHttpRequest();
+
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        console.log('Status: ' + httpRequest.status);
+        console.log(httpRequest.response);
+        }
+        else {
+            console.log(httpRequest.readyState);
+            
+        }
     
-    httpRequest.open("POST", messageurl);
+    httpRequest.open("POST", corsproxy+messageurl);
     httpRequest.setRequestHeader("Content-Type", "application/json");
-    httpRequest.send(JSON.stringify(jsonData));
+    httpRequest.send(JSON.stringify(messageData));
 }
